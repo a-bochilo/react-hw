63a45704b0376d9fd449c659d444bd481de08758
Alexander Bochilo <a.bochilo@mail.ru>
2022-08-24 15:47:10 +0300

    react hw7-8

--- src/App.js
+++ src/App.js
@@ -11,11 +11,11 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/about" element={<AboutUs />} />
-          <Route path="/servicing" element={<OurServices />} />
-          <Route path="/pricing" element={<PricingPlan />} />
+        <Route path="react-hw/" element={<Layout />}>
+          <Route path="react-hw/" element={<HomePage />} />
+          <Route path="react-hw/about" element={<AboutUs />} />
+          <Route path="react-hw//servicing" element={<OurServices />} />
+          <Route path="react-hw//pricing" element={<PricingPlan />} />
           <Route path="*" element={<ErrorPage />} />
         </Route>
       </Routes>
