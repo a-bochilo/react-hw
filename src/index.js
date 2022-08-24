c6550c4edf141a7778be29c6303578a9a18c54e6
Alexander Bochilo <a.bochilo@mail.ru>
2022-08-24 16:01:41 +0300

    react hw7-8

--- src/index.js
+++ src/index.js
@@ -3,15 +3,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { BrowserRouter, HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <HashRouter>
-    {/* <BrowserRouter> */}
+  <BrowserRouter>
     <React.StrictMode>
       <App />
     </React.StrictMode>
-    {/* </BrowserRouter> */}
-  </HashRouter>
+  </BrowserRouter>
 );
