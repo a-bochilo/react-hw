1ba5d2a20f16f8f5e7d73ab81cea7c9e455c5ea2
Alexander Bochilo <a.bochilo@mail.ru>
2022-08-24 15:44:13 +0300

    react hw7-8

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
+import ErrorPage from "./Pages/ErrorPage";
+import HomePage from "./Pages/HomePage";
+import Layout from "./Pages/Layout";
 import AboutUs from "./Templates/AboutUs";
-import Banner from "./Templates/Banner";
 import OurServices from "./Templates/OurServices";
 import PricingPlan from "./Templates/PricingPlan";
 
 function App() {
   return (
     <>
-      <Banner />
-      <AboutUs />
-      <OurServices />
-      <PricingPlan />
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/about" element={<AboutUs />} />
+          <Route path="/servicing" element={<OurServices />} />
+          <Route path="/pricing" element={<PricingPlan />} />
+          <Route path="*" element={<ErrorPage />} />
+        </Route>
+      </Routes>
     </>
   );
 }
---
+++ src/Components/NavBar/constants.js
@@ -0,0 +1,37 @@
+export const NAV_BAR_DATA = [
+  {
+    id: 0,
+    label: "Home",
+    url: "/",
+  },
+  {
+    id: 1,
+    label: "About",
+    url: "/about",
+  },
+  {
+    id: 2,
+    label: "Servicing",
+    url: "/servicing",
+  },
+  {
+    id: 3,
+    label: "Pricing",
+    url: "/pricing",
+  },
+  {
+    id: 4,
+    label: "Team",
+    url: "/team",
+  },
+  {
+    id: 5,
+    label: "Blog",
+    url: "/blog",
+  },
+  {
+    id: 6,
+    label: "Contact us",
+    url: "/contact-us",
+  },
+];
